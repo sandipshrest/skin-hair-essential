64cccc7f39f2277a0527b04c793823654082eca6
Sandip <c.restsandip123@gmail.com>
2024-02-29 12:59:06 +0545

    favicon added & added some meta keyword

--- src/pages/home/Feature.jsx
+++ src/pages/home/Feature.jsx
@@ -3,8 +3,8 @@ import FeatureData from "../../data/FeatureData";
 
 const Feature = () => {
   return (
-    <section className="relative pt-32">
-      <div className="container absolute z-10 -bottom-12 left-1/2 -translate-x-1/2 w-2/3 bg-white border border-gray-400 rounded-lg py-8 grid grid-cols-3">
+    <section className="relative pt-28">
+      <div className="container absolute z-10 -bottom-16 left-1/2 -translate-x-1/2 w-2/3 bg-white border border-gray-400 rounded-lg py-8 grid grid-cols-3">
         {FeatureData?.map((item, id) => (
           <div key={id} className="flex items-center gap-5">
             <div className="rounded-full bg-green-200 size-12 flex items-center justify-center">
