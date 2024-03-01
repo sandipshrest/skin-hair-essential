eba44ec1972fb56c7894b4b8328a1f60f20d2be2
Sandip <c.restsandip123@gmail.com>
2024-03-01 15:40:28 +0545

    install & setup redux toolkit and middleware

---
+++ src/redux/Store.jsx
@@ -0,0 +1,24 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import cartReducer from "../redux/reducerSlice/CartSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+import logger from "redux-logger";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const reducer = combineReducers({
+  cart: cartReducer,
+  //   wishlist: wishlistReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: () => [logger],
+});
+
+export const persistor = persistStore(store);
