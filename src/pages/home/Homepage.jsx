2aaa2e7eb87bc47c2e973b77ebc38e035abc84c2
Sandip <c.restsandip123@gmail.com>
2024-03-01 12:47:59 +0545

    add latest product section

--- src/pages/home/Homepage.jsx
+++ src/pages/home/Homepage.jsx
@@ -3,12 +3,14 @@ import Banner from "./Banner";
 import Feature from "./Feature";
 import FeatureProduct from "./FeatureProduct";
 import Testimonial from "./Testimonial";
+import Products from "./Products";
 
 const Homepage = () => {
   return (
     <>
       <Banner />
       <FeatureProduct />
+      <Products />
       <Testimonial />
       <Feature />
     </>
