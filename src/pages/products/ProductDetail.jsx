979311e47407c3fe4fed93237818b99ec127451d
Sandip <c.restsandip123@gmail.com>
2024-03-04 13:48:52 +0545

    add review section on product detail page

--- src/pages/products/ProductDetail.jsx
+++ src/pages/products/ProductDetail.jsx
@@ -36,47 +36,66 @@ const ProductDetail = () => {
         {productDetail && (
           <div className="container flex gap-14">
             <div className="w-1/3 px-6">
+              <div className="w-full sticky top-28">
               <img
                 src={productDetail.image}
                 alt={productName}
                 className="w-full h-[450px] object-contain bg-color2 bg-opacity-20"
               />
+              </div>
             </div>
-            <div className="w-2/3 flex flex-col gap-2">
-              <small className="text-base font-medium text-gray-700">
-                {productDetail.category}
-              </small>
-              <h2 className="text-4xl font-semibold">
-                {productDetail.productName}
-              </h2>
-              <p>{productDetail.desc}</p>
-              <p className="text-lg font-semibold">
-                Imported From: {productDetail.companyName}
-              </p>
-              <b className="text-xl font-bold">Price: {productDetail.price}</b>
-              <div className="flex items-center gap-3 mt-3 text-lg">
-                <button
-                  disabled={isItemInCart(productDetail)}
-                  onClick={() => dispatch(addToCart(productDetail))}
-                  className={`py-1 px-2 text-white transition-all duration-200 ease-linear ${
-                    isItemInCart(productDetail)
-                      ? "bg-green-500"
-                      : "bg-green-700"
-                  }`}
-                >
-                  Add To Cart
-                </button>
-                <button
-                  disabled={isItemInWishlist(productDetail)}
-                  onClick={() => dispatch(addToWishlist(productDetail))}
-                  className={`py-1 px-2 bg-color3 text-white transition-all duration-200 ease-linear ${
-                    isItemInWishlist(productDetail)
-                      ? "bg-opacity-80"
-                      : "bg-opacity-100"
-                  }`}
-                >
-                  Add To Wishlist
-                </button>
+            <div className="w-2/3 flex flex-col gap-12">
+              <div className="flex flex-col gap-2">
+                <small className="text-base font-medium text-gray-700">
+                  {productDetail.category}
+                </small>
+                <h2 className="text-4xl font-semibold">
+                  {productDetail.productName}
+                </h2>
+                <p>{productDetail.desc}</p>
+                <p className="text-lg font-semibold">
+                  Imported From: {productDetail.companyName}
+                </p>
+                <b className="text-xl font-bold">
+                  Price: {productDetail.price}
+                </b>
+                <div className="flex items-center gap-3 mt-3 text-lg">
+                  <button
+                    disabled={isItemInCart(productDetail)}
+                    onClick={() => dispatch(addToCart(productDetail))}
+                    className={`py-1 px-2 text-white transition-all duration-200 ease-linear ${
+                      isItemInCart(productDetail)
+                        ? "bg-green-500"
+                        : "bg-green-700"
+                    }`}
+                  >
+                    Add To Cart
+                  </button>
+                  <button
+                    disabled={isItemInWishlist(productDetail)}
+                    onClick={() => dispatch(addToWishlist(productDetail))}
+                    className={`py-1 px-2 bg-color3 text-white transition-all duration-200 ease-linear ${
+                      isItemInWishlist(productDetail)
+                        ? "bg-opacity-80"
+                        : "bg-opacity-100"
+                    }`}
+                  >
+                    Add To Wishlist
+                  </button>
+                </div>
+              </div>
+              <div className="bg-white w-full p-5 shadow-md space-y-5">
+                <h3 className="text-xl font-semibold">Give Feedback</h3>
+                <form className="w-1/2 space-y-1">
+                  <textarea
+                    name=""
+                    id=""
+                    rows="6"
+                    placeholder="Write something ..."
+                    className="border border-gray-500 p-1 rounded focus:outline-none w-full"
+                  ></textarea>
+                  <input type="submit" value="Submit" className="py-1 px-2 bg-green-700 font-medium border border-green-700 text-white" />
+                </form>
               </div>
             </div>
           </div>
