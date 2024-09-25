47efc958e6c8fcf9bbaa1acfd158091c0b10405c
Sandip <c.restsandip123@gmail.com>
2024-09-25 23:57:10 +0545

    setup aceternity ui for admin dashboard

--- src/components/adminSidebar/AdminSidebar.jsx
+++ src/components/adminSidebar/AdminSidebar.jsx
@@ -1,20 +1,27 @@
 import React, { useRef, useState } from "react";
 import { Link } from "react-router-dom";
+import { cn } from "../../lib/utils";
 
-const AdminSidebar = () => {
+const AdminSidebar = ({ open }) => {
   const [showDropdown, setShowDropdown] = useState(false);
   const dropdownRef = useRef(null);
   return (
-    <aside className={`w-[200px] px-4 py-16 bg-gray-200 h-full relative`}>
+    <aside className={`w-full px-4 py-16 h-full relative`}>
       <nav>
         <ul className="flex flex-col items-start gap-5 font-medium">
-          <li className="w-full">
+          <li
+            className={cn(
+              "w-full group/sidebar"
+            )}
+          >
             <Link className="w-full inline-block" to="/dashboard">
               Dashboard
             </Link>
           </li>
           <li
-            className="w-full"
+            className={cn(
+              "w-full group/sidebar"
+            )}
             onMouseEnter={() => setShowDropdown(true)}
             onMouseLeave={() => setShowDropdown(false)}
           >
@@ -26,7 +33,10 @@ const AdminSidebar = () => {
             >
               <ul className="flex flex-col items-start gap-2 p-4">
                 <li className="w-full">
-                  <Link className="w-full inline-block" to="/dashboard/productList">
+                  <Link
+                    className="w-full inline-block"
+                    to="/dashboard/productList"
+                  >
                     Product List
                   </Link>
                 </li>
@@ -48,17 +58,38 @@ const AdminSidebar = () => {
               </ul>
             </div>
           </li>
-          <li className="w-full">
+          <li
+            className={cn(
+              "w-full group/sidebar"
+            )}
+          >
+            <Link className="w-full inline-block" to="">
+              Feedbacks
+            </Link>
+          </li>
+          <li
+            className={cn(
+              "w-full group/sidebar"
+            )}
+          >
             <Link className="w-full inline-block" to="">
               Customers
             </Link>
           </li>
-          <li className="w-full">
+          <li
+            className={cn(
+              "w-full group/sidebar"
+            )}
+          >
             <Link className="w-full inline-block" to="">
               Order
             </Link>
           </li>
-          <li className="w-full">
+          <li
+            className={cn(
+              "w-full group/sidebar"
+            )}
+          >
             <Link className="w-full inline-block" to="">
               Marketing
             </Link>
--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -1,21 +1,146 @@
-import React, { useState } from "react";
-import { Outlet } from "react-router-dom";
+import { cn } from "../lib/utils";
+import React, { useState, createContext, useContext } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { Link, Outlet } from "react-router-dom";
+import { BsDash } from "react-icons/bs";
+import { BiArrowFromLeft, BiMenu, BiUser } from "react-icons/bi";
+import { CiSettings } from "react-icons/ci";
+import { FaXmark } from "react-icons/fa6";
 import AdminHeader from "../components/adminHeader/AdminHeader";
 import AdminSidebar from "../components/adminSidebar/AdminSidebar";
 
-const AdminLayout = () => {
+const SidebarContext = createContext(undefined);
+
+export const useSidebar = () => {
+  const context = useContext(SidebarContext);
+  if (!context) {
+    throw new Error("useSidebar must be used within a SidebarProvider");
+  }
+  return context;
+};
+
+export const SidebarProvider = ({
+  children,
+  open: openProp,
+  setOpen: setOpenProp,
+  animate = true,
+}) => {
+  const [openState, setOpenState] = useState(false);
+
+  const open = openProp !== undefined ? openProp : openState;
+  const setOpen = setOpenProp !== undefined ? setOpenProp : setOpenState;
+
   return (
-    <div className="h-screen flex">
-      <AdminSidebar />
-      <div className={`flex flex-col flex-1 h-full `}>
-        <AdminHeader
-        />
-        <div className="p-6 w-full">
-          <Outlet />
-        </div>
-      </div>
-    </div>
+    <SidebarContext.Provider value={{ open, setOpen, animate: animate }}>
+      {children}
+    </SidebarContext.Provider>
   );
 };
 
-export default AdminLayout;
+export const Sidebar = ({ children, open, setOpen, animate }) => {
+  return (
+    <SidebarProvider open={open} setOpen={setOpen} animate={animate}>
+      {children}
+    </SidebarProvider>
+  );
+};
+
+export const SidebarBody = (props) => {
+  return (
+    <>
+      <DesktopSidebar {...props} />
+      <MobileSidebar {...props} />
+    </>
+  );
+};
+
+// for desktop view
+export const DesktopSidebar = ({ className, children, ...props }) => {
+  const { open, setOpen, animate } = useSidebar();
+  return (
+    <>
+      <motion.div
+        className={cn(
+          "h-full px-4 py-4 hidden  md:flex md:flex-col bg-white border-r border-gray-300 w-[300px] flex-shrink-0",
+          className
+        )}
+        animate={{
+          width: animate ? (open ? "300px" : "80px") : "300px",
+        }}
+        onMouseEnter={() => setOpen(true)}
+        onMouseLeave={() => setOpen(false)}
+        {...props}
+      >
+        {children}
+      </motion.div>
+    </>
+  );
+};
+
+// for mobile view
+export const MobileSidebar = ({ className, children, ...props }) => {
+  const { open, setOpen } = useSidebar();
+  return (
+    <>
+      <div
+        className={cn(
+          "h-10 px-4 py-4 flex flex-row md:hidden  items-center justify-between bg-neutral-100 w-auto sticky top-0 z-10"
+        )}
+        {...props}
+      >
+        <div className="flex justify-end w-full">
+          <div className="text-neutral-800" onClick={() => setOpen(!open)}>
+            Open
+          </div>
+        </div>
+        <AnimatePresence>
+          {open && (
+            <motion.div
+              initial={{ x: "-100%", opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              exit={{ x: "-100%", opacity: 0 }}
+              transition={{
+                duration: 0.3,
+                ease: "easeInOut",
+              }}
+              className={cn(
+                "fixed h-full w-[300px] inset-0 bg-white py-6 px-4 z-[100] flex flex-col justify-between shadow-md",
+                className
+              )}
+            >
+              <div
+                className="absolute right-10 top-10 z-50 text-neutral-800"
+                onClick={() => setOpen(!open)}
+              >
+                Close
+              </div>
+              {children}
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </div>
+    </>
+  );
+};
+
+export default function AdminLayout() {
+  const [open, setOpen] = useState(false);
+  return (
+    <div
+      className={cn(
+        "rounded-md flex flex-col md:flex-row bg-white w-full flex-1 mx-auto border border-neutral-200 overflow-hidden",
+        "h-screen"
+      )}
+    >
+      <Sidebar open={open} setOpen={setOpen}>
+        <SidebarBody className="justify-between gap-10">
+          <AdminSidebar open={open} />
+        </SidebarBody>
+      </Sidebar>
+      <div className="flex flex-col w-full">
+        <AdminHeader />
+        <Outlet />
+      </div>
+    </div>
+  );
+}
